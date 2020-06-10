a82f5b915296dac0ef4a34df841e7d5e6b47edc3
MrRadix <edo.ermini@gmail.com>
2020-06-10 16:14:54 +0200

    added better table formatting

--- analisi.sh
+++ analisi.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-echo " ------------------------------------------------------------------------------------------------------------ "
-printf "| %-20s | %-20s | %-20s | %-20s | %s |\n" "ClIENT ID" "N PRODUCTS" "TIME IN SUPERMARKET" "TIME IN QUEUE" "N QUEUE VIEWED"
-echo "|------------------------------------------------------------------------------------------------------------|"
+echo "┌──────────────────────┬──────────────────────┬──────────────────────┬──────────────────────┬────────────────┐"
+printf "│ %-20s │ %-20s │ %-20s │ %-20s │ %s │\n" "ClIENT ID" "N PRODUCTS" "TIME IN SUPERMARKET" "TIME IN QUEUE" "N QUEUE VIEWED"
+echo "├──────────────────────┼──────────────────────┼──────────────────────┼──────────────────────┼────────────────┤"
 
 while IFS=' ' read -r t id sm_time q_time q_viewed n_prod; do
 
@@ -12,16 +11,16 @@ while IFS=' ' read -r t id sm_time q_time q_viewed n_prod; do
 
     sm_time_sec=$(echo "scale=3; $sm_time / 1000" | bc)
     q_time_sec=$(echo "scale=3; $q_time / 1000" | bc)
-    printf "| %-20s | %-20s | %-20s | %-20s | %-14s |\n" "$id" "$n_prod" "$sm_time_sec" "$q_time_sec" "$q_viewed"
+    printf "│ %-20s │ %-20s │ %-20s │ %-20s │ %-14s │\n" "$id" "$n_prod" "$sm_time_sec" "$q_time_sec" "$q_viewed"
 
 done < $1
-echo " ------------------------------------------------------------------------------------------------------------ "
+echo "└──────────────────────┴──────────────────────┴──────────────────────┴──────────────────────┴────────────────┘"
 
 echo 
 echo
-echo " ------------------------------------------------------------------------------------------------------------ "
-printf "| %-15s | %-15s | %-15s | %-15s | %-21s | %s |\n" "CASH ID" "N PRODUCTS" "N CLIENTS" "TOT TIME OPEN" "AVERAGE SERVICE TIME" "N CLOSINGS"
-echo "|------------------------------------------------------------------------------------------------------------|"
+echo "┌─────────────────┬─────────────────┬─────────────────┬─────────────────┬───────────────────────┬────────────┐"
+printf "│ %-15s │ %-15s │ %-15s │ %-15s │ %-21s │ %s │\n" "CASH ID" "N PRODUCTS" "N CLIENTS" "TOT TIME OPEN" "AVERAGE SERVICE TIME" "N CLOSINGS"
+echo "├─────────────────┼─────────────────┼─────────────────┼─────────────────┼───────────────────────┼────────────┤"
 
 while IFS=' ' read -r t id n_prod n_clients n_closings t_4_clients t_4_openings; do 
 
@@ -53,7 +52,7 @@ while IFS=' ' read -r t id n_prod n_clients n_closings t_4_clients t_4_openings;
         open_time=$(echo "scale=3; $open_time / 1000" | bc)
     fi
     
-    printf "| %-15s | %-15s | %-15s | %-15s | %-21s | %-10s |\n" "$id" "$n_prod" "$n_clients" "$open_time" "$service_avg" "$n_closings"
+    printf "│ %-15s │ %-15s │ %-15s │ %-15s │ %-21s │ %-10s │\n" "$id" "$n_prod" "$n_clients" "$open_time" "$service_avg" "$n_closings"
 
 done < $1
-echo " ------------------------------------------------------------------------------------------------------------"
+echo "└─────────────────┴─────────────────┴─────────────────┴─────────────────┴───────────────────────┴────────────┘"
