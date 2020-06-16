5e0d96b308d82088564829f86d70aa9b6998b744
MrRadix <edo.ermini@gmail.com>
2020-06-16 10:29:06 -0400

    improved configuration error messages

--- src/supermarket.c
+++ src/supermarket.c
@@ -170,62 +170,74 @@ int main(int argc, char const *argv[])
      * check of parameters values
      */
     if (k < 1) {
-        fprintf(stderr, "K must be > 1\n");
+        fprintf(stderr, "Configuration ERROR: K must be > 1\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (init_k_n < 1 || init_k_n > k) {
-        fprintf(stderr, "INITKN mut be set between 1 and K\n");
+        fprintf(stderr, "Configuration ERROR: INITKN mut be set between 1 and K\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (c < 1) {
-        fprintf(stderr, "C must be set greather than 1\n");
+        fprintf(stderr, "Configuration ERROR: C must be set greather than 1\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (e < 1 || e > c) {
-        fprintf(stderr, "E mut be set between 1 and C\n");
+        fprintf(stderr, "Configuration ERROR: E mut be set between 1 and C\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (t <= 10) {
-        fprintf(stderr, "T mut be set greater than 10\n");
+        fprintf(stderr, "Configuration ERROR: T mut be set greater than 10\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (p < 0) {
-        fprintf(stderr, "P must be set greater or equal than 0\n");
+        fprintf(stderr, "Configuration ERROR: P must be set greater or equal than 0\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (prod_time <= 0) {
-        fprintf(stderr, "PRODTIME must be set greater than 0\n");
+        fprintf(stderr, "Configuration ERROR: PRODTIME must be set greater than 0\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (analytics_intervall <= 0) {
-        fprintf(stderr, "ANALYTICS_T must be set greater than 0\n");
+        fprintf(stderr, "Configuration ERROR: ANALYTICS_T must be set greater than 0\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (analytics_diff <= 0) {
-        fprintf(stderr, "ANALYTICS_DIFF must be set greater than 0\n");
+        fprintf(stderr, "Configuration ERROR: ANALYTICS_DIFF must be set greater than 0\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (log_filename == NULL || strcmp(log_filename, "") == 0) {
-        fprintf(stderr, "LOG_FN need to be set\n");
+        fprintf(stderr, "Configuration ERROR: LOG_FN need to be set\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (s1 < 1 || s1 > k) {
-        fprintf(stderr, "S1 must be set between 1 and K\n");
+        fprintf(stderr, "Configuration ERROR: S1 must be set between 1 and K\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
     if (s2 < 1 || s2 > c) {
-        fprintf(stderr, "S2 must be between 1 and C\n");
+        fprintf(stderr, "Configuration ERROR: S2 must be between 1 and C\n");
+        fprintf(stderr, "Quitting...\n");
         free(log_filename);
         exit(EXIT_FAILURE);
     }
